61d6e424829da515437d17c1fa0392025764110d
thanakornpasangthien <mingbackmountain@gmail.com>
2020-01-01 01:21:43 +0700

    Finish project

---
+++ src/Box.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { choice } from "./helper";
+import "./Box.css";
+
+class Box extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: choice(this.props.colors) };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  pickColor() {
+    let newColor;
+    do {
+      newColor = choice(this.props.colors);
+    } while (newColor === this.state.color);
+
+    this.setState({ color: newColor });
+  }
+  handleClick() {
+    this.pickColor();
+  }
+  render() {
+    return (
+      <div
+        className='Box'
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}
+      ></div>
+    );
+  }
+}
+
+export default Box;
